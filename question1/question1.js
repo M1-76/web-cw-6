8aec06f71015c523147a45718fefb5d124eaebb3
Mohammed Elsamad M1-76 <mohammed.elsamadss@gmail.com>
2021-01-03 21:38:58 +0300

    done

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,12 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+let name = prompt( ` Whats Your Name?`)
+console.log(name)
+function sayHello() {
+    alert( ` Hello ${name}`);
+
+}
+if( name = ` `) {
+    sayHello()
+} else{
+    alert(` invalid Input`)
+}
